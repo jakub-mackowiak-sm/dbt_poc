f3f8c446e1fe63fad355d7001ca0faf52fc6510e
Jakub Mackowiak <jakub.mackowiak@softwaremind.com>
2025-12-02 10:31:56 +0000

    first version

---
+++ models/agg_customer_orders.sql
@@ -0,0 +1,15 @@
+WITH customer_orders as (
+
+    select
+        customer_id,
+
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders
+
+    from {{ ref('raw_orders') }}
+
+    group by 1
+
+)
+select * from customer_orders
